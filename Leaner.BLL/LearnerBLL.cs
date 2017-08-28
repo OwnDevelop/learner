bc749497495025422548f2b2e7dc6e54e175bb14
Morenkov <ivanmorenkov@mail.ru>
2017-08-29 02:44:57 +0400

    Доделаны ссылки, переработан UI, начата реализация импорта файлов через диалоговое окно в строковую переменную

--- Leaner.BLL/LearnerBLL.cs
+++ Leaner.BLL/LearnerBLL.cs
@@ -13,7 +13,7 @@ namespace Leaner.BLL
 {
     public class LearnerBLL : ILearnerBLL
     {
-        ILearnerDAL dal; //cheeck in commit
+        ILearnerDAL dal;
 
         public LearnerBLL()
         {
@@ -44,7 +44,7 @@ namespace Leaner.BLL
             }
             catch (Exception e)
             {
-                throw new UnknowException("Unknow exception", e);
+                throw new UnknownException("Unknow exception", e);
             }
         }
 
@@ -65,7 +65,7 @@ namespace Leaner.BLL
             }
             catch (Exception e)
             {
-                throw new UnknowException("Unknow exception", e);
+                throw new UnknownException("Unknown exception", e);
             }
         }
 
@@ -85,7 +85,7 @@ namespace Leaner.BLL
             }
             catch (Exception e)
             {
-                throw new UnknowException("Unknow exception", e);
+                throw new UnknownException("Unknown exception", e);
             }
         }
     }
